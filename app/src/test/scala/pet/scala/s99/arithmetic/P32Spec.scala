251d785f5df9e82b12e2eb0d8e6d1b1017096629
Imre Molnar <imre.molnar.0225@gmail.com>
2021-03-28 12:21:10 +0200

    Solution for P32

---
+++ app/src/test/scala/pet/scala/s99/arithmetic/P32Spec.scala
@@ -0,0 +1,22 @@
+package pet.scala.s99.arithmetic
+
+import pet.scala.s99.PxxSpec
+
+class P32Spec extends PxxSpec {
+  private def gcd(a: Int, b: Int): Int =
+    if (b == 0) a
+    else gcd(b, a % b)
+  
+  behavior of "'gcd' method"
+
+  it should "determine the greatest common divisor of two positive integers" in {
+    gcd(8, 12) shouldBe 4
+    gcd(36, 63) shouldBe 9
+    gcd(12, 8) shouldBe 4
+    gcd(63, 36) shouldBe 9
+  }
+
+  it should "determine the greatest common divisor of two huge integers" in {
+    gcd(999999,909090) shouldBe 90909
+  }
+}
